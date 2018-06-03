3bffc0748fdbe174e7be1296fc44357ca4efc648
Tony Scavone <ascavone@mediamath.com>
2018-06-03 16:24:54 -0400

    actor 2

--- src/holdfast/actor.cljs
+++ src/holdfast/actor.cljs
@@ -1,5 +1,9 @@
-(ns holdfast.actors1
-  (:require [clojure.test :refer :all]))
+(ns holdfast.actors2
+  (:require [clojure.test :refer :all]
+             [clojure.spec.alpha :as s]
+             [clojure.spec.test.alpha :as stest]))
+
+(def NAME_LENGTH_LIMIT 85)
 
 (defprotocol Agent
   "an actor in holdfast; enemy, user or npc"
@@ -8,13 +12,30 @@
 (defrecord Actor [name max-hp hp job]
   Agent (is-alive? [this] (>= (:hp this) 0)))
 
+(s/def ::name (s/and string? #(< (count(:name %)) NAME_LENGTH_LIMIT)))
+(s/def ::max-hp (s/and int? #(> % 0)))
+(s/def ::hp int?)
+(s/def ::type #{:player :flies :npc})
+(s/def ::job #{:tiller :herdsman :nurse :parson :pilgrim})
+(s/def ::actor (s/cat :name (s/or ::npc_name ::name)
+                      :hp ::hp
+                      :max-hp ::max-hp
+                      :job ::job))
+(s/fdef ->Actor
+        :args ::actor
+        :ret Actor)
+
 (deftest create-actor
   (testing "create an actor record"
-    (is (not (nil? (->Actor :player 25 25 :pilgrim))))))
+    (is (not (nil? (->Actor "cloud" 25 25 :pilgrim))))))
 
 (deftest is-alive
   (testing "is alive"
-    (let [cloud (->Actor :player 25 25 :pilgrim)]
+    (let [cloud (->Actor "cloud" 25 -1 :pilgrim)]
       (is (true? (is-alive? cloud))))))
 
-;; https://stackoverflow.com/questions/4575170/where-should-i-use-defrecord-in-clojure
+;(deftest actor-spec
+;  (testing "hp can't be negative"
+;    (is (thrown? Exception (->Actor -1 25 :nurse)))))
+
+(stest/instrument `->Actor)
